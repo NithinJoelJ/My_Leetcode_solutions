ddb9d78ac4d73b8d2ef09e5309a81f66cf0a2a87
Nithin Joel J <141165454+NithinJoelJ@users.noreply.github.com>
2025-09-07 09:10:17 +0530

    Time: 1 ms (99.88%), Space: 46.1 MB (13.65%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int last_position = nums.length-1;
+        for(int i=nums.length-2; i>=0; i--){
+            if(i+nums[i]>=last_position){
+                last_position = i;
+            }
+        }
+        return last_position==0;
+    }
+}
